103016d8a47a5c671a00c96110c50d70697985bd
tapuks <david_berdiell@hotmail.com>
2021-04-24 11:09:32 +0000

    edit 1

--- src/js/index.js
+++ src/js/index.js
@@ -33,7 +33,7 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
+          <h1>${variables.name == null ? "" : variables.name}</h1>
           <h2>Web Developer</h2>
           <h3>Miami, USA</h3>
           <ul class="position-right">
@@ -77,6 +77,7 @@ window.onload = function() {
       // <- add a listener to every input
       const attribute = e.target.getAttribute("for"); // when any input changes, collect the value
       let values = {};
+      console.log(attribute);
       values[attribute] =
         this.value == "" || this.value == "null"
           ? null
